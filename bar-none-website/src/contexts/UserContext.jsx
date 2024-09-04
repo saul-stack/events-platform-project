1dbc9b9de91e184fad2255a285c608456a877216
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-04 16:49:04 +0100

    feat- Add UserContext.jsx for global user state Added UserContext to manage global user state. This will be used to store user information and authentication state (is the user logged in and as who?)

---
+++ bar-none-website/src/contexts/UserContext.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const UserContext = React.createContext({ user: {} });
+
+const UserProvider = ({ children }) => {
+  const testUser = {
+    name: "Test User",
+  };
+
+  const [user, setUser] = useState(testUser);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserContext, UserProvider };
