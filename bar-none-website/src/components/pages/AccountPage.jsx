2f26877e17433b589aa8288c79a532bb1eb3ae23
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-04 14:20:41 +0100

    ui- modify AccountPage.jsx

--- bar-none-website/src/components/pages/AccountPage.jsx
+++ bar-none-website/src/components/pages/AccountPage.jsx
@@ -1,4 +1,13 @@
 function AccountPage() {
-  return <h1>AccountPage is here</h1>;
+  return (
+    <div>
+      <h1>AccountPage is here</h1>
+      <p>
+        This area will display whether the user is logged in or not. If the User
+        is logged in, it should also display their login name in the Navbar next
+        to the Account button..
+      </p>
+    </div>
+  );
 }
 export default AccountPage;
