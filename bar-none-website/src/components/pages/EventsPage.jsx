abcdd5387086b73c5233154b858765b8b78aced1
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-04 14:32:21 +0100

    ui- update EventsPage.jsx

--- bar-none-website/src/components/pages/EventsPage.jsx
+++ bar-none-website/src/components/pages/EventsPage.jsx
@@ -1,4 +1,13 @@
 function EventsPage() {
-  return <h1>EventsPage is here</h1>;
+  return (
+    <div>
+      <h1>EventsPage is here</h1>
+      <p>
+        This page will display upcoming events as cards in a grid. Possible user
+        interactions include viewing details about the event, adding it to
+        "my-calendar" and purchasing tickets.
+      </p>
+    </div>
+  );
 }
 export default EventsPage;
