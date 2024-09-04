4bccffd15eec981b17b26f66cc22614c3c792ec1
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-04 18:49:46 +0100

    dev- remove commented code from LoginPage.jsx Removed commeted out password form option from the LoginPage component. It will be reintroduced in future but is not needed at this stage.

--- bar-none-website/src/components/pages/LoginPage.jsx
+++ bar-none-website/src/components/pages/LoginPage.jsx
@@ -13,10 +13,6 @@ function LoginPage() {
           <label htmlFor="username">Username:</label>
           <input type="text" id="username" name="username" required />
         </div>
-        {/* <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
-        </div> */}
         <button type="submit">Login</button>
       </form>
     </div>
