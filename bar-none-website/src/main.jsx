bdf5f6ebd319cc5a0709bcbc3f80aa039fde8d85
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-04 16:52:58 +0100

    feat- wrap App.jsx in UserContext.Provider Wrapped App.jsx in UserContext.Provider to provide user data to all child components.

--- bar-none-website/src/main.jsx
+++ bar-none-website/src/main.jsx
@@ -2,14 +2,17 @@ import "./styles/index.css";
 
 import React, { StrictMode } from "react";
 
-import App from "./components/App.jsx";
-import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App.jsx";
+import { UserProvider } from "./contexts/UserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </StrictMode>
   </BrowserRouter>
 );
