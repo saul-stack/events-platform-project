068c30338efa98d23e00301347769b4612887cfd
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-05 14:44:58 +0100

    feat: update seed-db to insert data from json file Using fs to read data from a .JSON file "test-data.json" and insert it into the database.

--- backend/database/seed/seed-db.js
+++ backend/database/seed/seed-db.js
@@ -1,7 +1,10 @@
 const eventsAndUsersPool = require("../connection");
+const fs = require("fs").promises;
 
 const seedDatabase = async () => {
   try {
+    const eventsData = await fs.readFile("./data/test-data.json", "utf8");
+    const seedData = JSON.parse(eventsData);
     await eventsAndUsersPool.query(`
       DROP TABLE IF EXISTS events;
     `);
@@ -18,13 +21,35 @@ const seedDatabase = async () => {
         advance_price DECIMAL(5, 2),
         door_price DECIMAL(5, 2),
         total_tickets INT,
-        tickets_available INT,
+        tickets_sold INT,
         is_seated BOOLEAN NOT NULL,
         is_ticketed BOOLEAN NOT NULL,
         is_recurring BOOLEAN NOT NULL
       );
     `);
-    console.log("Create 'events' table \u2714");
+    console.log("Create empty 'events' table with columns \u2714");
+
+    for (const event of seedData) {
+      await eventsAndUsersPool.query(
+        `INSERT INTO events (
+          name, date, day_of_week, time, description, advance_price, door_price, total_tickets, tickets_sold, is_seated, is_ticketed, is_recurring
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
+        [
+          event.name,
+          event.date,
+          event.day_of_week,
+          event.time,
+          event.description,
+          event.advance_price,
+          event.door_price,
+          event.total_tickets,
+          event.tickets_sold,
+          event.is_seated,
+          event.is_ticketed,
+          event.is_recurring,
+        ]
+      );
+    }
 
     const result = await eventsAndUsersPool.query(
       "SELECT COUNT(*) FROM events"
@@ -36,7 +61,7 @@ const seedDatabase = async () => {
   } catch (err) {
     console.error("Error seeding database:", err);
   } finally {
-    eventsAndUsersPool.end();
+    await eventsAndUsersPool.end();
   }
 };
 
