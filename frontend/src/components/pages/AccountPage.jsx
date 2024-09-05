a1c5058eee6f62146c12fc0bf52b2f76eb18b9f7
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-05 11:20:02 +0100

    feat: add Logout button to AccountPage.jsx Added a logout button. It updates UserContext -> user.name to null and navigates to /login.

--- frontend/src/components/pages/AccountPage.jsx
+++ frontend/src/components/pages/AccountPage.jsx
@@ -1,13 +1,21 @@
-import { useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 function AccountPage() {
-  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+  const { user, setUser } = useContext(UserContext);
+
+  function handleLogout() {
+    setUser({ name: null });
+    navigate("/login");
+  }
+
   return (
     <div>
-      <h1>AccountPage is here</h1>
       <p>
         Logged in as: <b>{user.name}</b>
       </p>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
