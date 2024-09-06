ec91d3036dc3b5752062dc4b2f467a3869c6d206
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-06 19:52:30 +0100

    refactor/feat: replace http module with Express Switched from http module to Express.js. The server also now responds with a message containing the request method and URL for any method or url.

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,19 +1,17 @@
-const http = require("http");
-const port = 9090;
+const express = require("express");
+const PORT = 9090;
 
-const server = http.createServer((request, response) => {
-  const requestUrl = request.url;
-  const requestMethod = request.method;
-  console.log(`Received a ${requestMethod} request on ${port}${requestUrl}`);
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(
-    JSON.stringify({ endpoint: request.url, method: request.method })
-  );
+const server = express();
+
+server.use((request, response) => {
+  const { method, url } = request;
+  console.log(`Received a ${method} request on ${url}`);
+  response.send(`Received your ${method} request on ${url}. Thank you!`);
 });
 
-server.listen(port, (error) => {
+server.listen(PORT, (error) => {
   if (error) console.log(error);
   else {
-    console.log(`Server is listening on port ${port}`);
+    console.log(`Server is listening on port ${PORT}`);
   }
 });
