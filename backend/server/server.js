93a228111f9a9b0503b1c2c14e61c302d8961767
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-12 20:14:06 +0100

    feat: add rejectRequestMethod middleware function Added a utility function to reject any request with a disallowed method in the server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -3,5 +3,20 @@ const express = require("express");
 const server = express();
 const { getAllEndpoints } = require("../MVC/controllers/api.controllers.js");
 
-server.use("/api", getAllEndpoints);
+const rejectRequestMethod = (req, res) => {
+  console.log("hi");
+  const METHOD = req.method;
+  const ENDPOINT = req.originalUrl;
+  console.error(` ${METHOD} Method Not Allowed on ${ENDPOINT}`);
+  return res
+    .status(405)
+    .json({ error: `${METHOD} Method Not Allowed on ${ENDPOINT}` });
+};
+
+server.get("/api", getAllEndpoints);
+server.delete("/api", rejectRequestMethod);
+server.put("/api", rejectRequestMethod);
+server.post("/api", rejectRequestMethod);
+server.patch("/api", rejectRequestMethod);
+
 module.exports = server;
