5935ac5718180142ba0b8862ff60222c0b65e2a7
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-13 10:52:00 +0100

    data: update getAllEndpoints method Moved the method filtering to the server.js

--- backend/MVC/controllers/api.controllers.js
+++ backend/MVC/controllers/api.controllers.js
@@ -1,13 +1,6 @@
 const { fetchAllEndpoints } = require("../models/api.models");
 
 exports.getAllEndpoints = async (req, res) => {
-  const METHOD = req.method;
-  if (METHOD !== "GET") {
-    console.error(` ${METHOD} Method Not Allowed on /api`);
-    return res
-      .status(405)
-      .json({ error: `${METHOD} Method Not Allowed on /api` });
-  }
   try {
     const endpoints = await fetchAllEndpoints();
     return res.status(200).json(endpoints);
