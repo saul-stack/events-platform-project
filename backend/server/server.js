18ced308850478218cb7bdfa4e67464258d345c8
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-13 22:47:44 +0100

    server: incl. body-parser middleware in server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const server = express();
+
 const { getAllEndpoints } = require("../MVC/controllers/api.controllers.js");
 const { getAllEvents } = require("../MVC/controllers/events.controllers.js");
+const { postToEvents } = require("../MVC/controllers/events.controllers.js");
 
 const rejectRequestMethod = (req, res) => {
   const METHOD = req.method;
@@ -13,6 +15,9 @@ const rejectRequestMethod = (req, res) => {
     .json({ error: `${METHOD} Method Not Allowed on ${ENDPOINT}` });
 };
 
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+
 server.get("/api", getAllEndpoints);
 server.delete("/api", rejectRequestMethod);
 server.put("/api", rejectRequestMethod);
@@ -20,6 +25,7 @@ server.post("/api", rejectRequestMethod);
 server.patch("/api", rejectRequestMethod);
 
 server.get("/api/events", getAllEvents);
+server.post("/api/events", postToEvents);
 server.delete("/api/events", rejectRequestMethod);
 server.put("/api/events", rejectRequestMethod);
 server.patch("/api/events", rejectRequestMethod);
