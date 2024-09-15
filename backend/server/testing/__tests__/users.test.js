f9a8cebadd3ced7c43503e3a059327def981b67d
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-15 23:06:38 +0100

    test: update users.test.js for /api/users

--- backend/server/testing/__tests__/users.test.js
+++ backend/server/testing/__tests__/users.test.js
@@ -30,4 +30,41 @@ describe("/api/users", () => {
     expect(response.status).toBe(200);
     expect(String(response.body.users)).toEqual(String(defaultUsersArray));
   });
+
+  test("DELETE: responds (405) with error message", async () => {
+    const response = await request(server).delete("/api/users");
+    expect(response.status).toBe(405);
+    expect(response.body.error).toBe("DELETE Method Not Allowed on /api/users");
+  });
+
+  test("PUT: responds (405) with error message", async () => {
+    const response = await request(server).put("/api/users");
+    expect(response.status).toBe(405);
+    expect(response.body.error).toBe("PUT Method Not Allowed on /api/users");
+  });
+
+  test("PATCH: responds (405) with error message", async () => {
+    const response = await request(server).patch("/api/users");
+    expect(response.status).toBe(405);
+    expect(response.body.error).toBe("PATCH Method Not Allowed on /api/users");
+  });
+
+  test("POST: responds (201) and successfully updates table", async () => {
+    const updatedUsersArray = defaultUsersArray;
+    updatedUsersArray.push(newUser);
+
+    const response = await request(server).post("/api/users").send(newUser);
+    const updatedUsersData = await fetchTable("users");
+
+    const userExists = updatedUsersData.some(
+      (user) =>
+        user.title === newUser.title && user.description === newUser.description
+    );
+
+    expect(response.status).toBe(201);
+    expect(response.body.message).toBe(
+      `User posted successfully: ${newUser.title}`
+    );
+    expect(userExists).toBe(true);
+  });
 });
