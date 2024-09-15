5093b9705f392487a0279d546222cb0e538f4def
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-15 22:04:15 +0100

    refactor: move newEvent to index.js

--- backend/server/testing/__tests__/events.test.js
+++ backend/server/testing/__tests__/events.test.js
@@ -1,21 +1,13 @@
-const { server, request, db } = require("./index.js");
-const { fetchTable, fetchTableEntry, seedTestTable } = require("./index.js");
+const {
+  server,
+  request,
+  db,
+  fetchTable,
+  fetchTableEntry,
+  seedTestTable,
+  newEvent,
+} = require("./index.js");
 
-//prettier-ignore
-const newEvent = {
-  "title": "POST Test Event",
-  "date": "2022-12-31",
-  "day_of_week": "Saturday",
-  "time": "18:00:00",
-  "description": "This is a test event created by a POST request.",
-  "advance_price": "10.0",
-  "door_price": "15.0",
-  "tickets_total": "100",
-  "tickets_sold": "0",
-  "is_seated": "true",
-  "is_ticketed": "true",
-  "is_recurring": "false"
-};
 let defaultEventsArray = [];
 
 beforeAll(async () => {
--- backend/server/testing/__tests__/index.js
+++ backend/server/testing/__tests__/index.js
@@ -8,6 +8,21 @@ const {
 } = require("../../../database/db-utils.js");
 const { seedTestTable } = require("../../../database/db-utils.js");
 
+const newEvent = {
+  "title": "POST Test Event",
+  "date": "2022-12-31",
+  "day_of_week": "Saturday",
+  "time": "18:00:00",
+  "description": "This is a test event created by a POST request.",
+  "advance_price": "10.0",
+  "door_price": "15.0",
+  "tickets_total": "100",
+  "tickets_sold": "0",
+  "is_seated": "true",
+  "is_ticketed": "true",
+  "is_recurring": "false",
+};
+
 module.exports = {
   server,
   request,
@@ -15,4 +30,5 @@ module.exports = {
   fetchTable,
   fetchTableEntry,
   seedTestTable,
+  newEvent,
 };
