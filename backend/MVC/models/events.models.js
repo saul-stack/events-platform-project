67c360875418b4c721b38d7b8a2aa3762af25e04
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-15 23:09:25 +0100

    refactor: update variable names in events model

--- backend/MVC/models/events.models.js
+++ backend/MVC/models/events.models.js
@@ -21,11 +21,11 @@ const postEvent = async (newEvent) => {
       throw new Error("Table does not exist");
     }
 
-    const singleEntryValues = extractValues(newEvent);
+    const values = extractValues(newEvent);
 
     await db.query(
       "INSERT INTO events (title, date, day_of_week, time, description, advance_price, door_price, tickets_total, tickets_sold, is_seated, is_ticketed, is_recurring) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)",
-      singleEntryValues
+      values
     );
   } catch (error) {
     console.error("Error inserting data:", error);
