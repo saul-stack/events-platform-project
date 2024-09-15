9b1a7894706ee57299ed4accc601bc22f45df29d
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-15 17:03:29 +0100

    feat: render events from database on eventsPage

--- frontend/src/components/pages/EventsPage.jsx
+++ frontend/src/components/pages/EventsPage.jsx
@@ -1,4 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+import { getAllEvents } from "../../../api-functions";
+
 function EventsPage() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchAllEvents = async () => {
+      const result = await getAllEvents();
+      console.log("result", result);
+      setEvents(result);
+    };
+
+    fetchAllEvents();
+  }, []);
+
   return (
     <div>
       <h1>EventsPage is here</h1>
@@ -7,7 +23,19 @@ function EventsPage() {
         interactions include viewing details about the event, adding it to
         "my-calendar" and purchasing tickets.
       </p>
+      <div>
+        {events.map((event) => (
+          <div key={event.id}>
+            <h2>{event.title}</h2>
+            <p>{event.description}</p>
+            <p>{event.date}</p>
+            <p>{event.time}</p>
+            <p>{event.location}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+
 export default EventsPage;
