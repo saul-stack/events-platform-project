fd8c68a7a89acd10555f9880b3a120b9a93ef664
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-16 22:41:06 +0100

    refactor: refactor db-utils functions

--- backend/database/db-utils.js
+++ backend/database/db-utils.js
@@ -2,93 +2,60 @@ const db = require("./connection");
 const fs = require("fs").promises;
 const path = require("path");
 
-const seedTestTable = async (tableName) => {
-  let tableDataPath;
-  switch (tableName) {
-    case "events":
-      tableDataPath = path.join(__dirname, "/test-data/events-test-data.json");
-      break;
-    case "users":
-      tableDataPath = path.join(__dirname, "/test-data/users-test-data.json");
-      break;
-    default:
-      console.error(`Unknown table name: ${tableName}`);
-      return;
-  }
+const handleError = (message, error) => {
+  console.error(`${message}:`, error);
+  throw error;
+};
 
-  let tableData = null;
+const fetchJson = async (filePath) => {
   try {
-    tableData = await fetchJson(tableDataPath);
+    const data = await fs.readFile(filePath, "utf-8");
+    return JSON.parse(data);
   } catch (error) {
-    console.error(`Error reading JSON file: ${error}`);
-    return error;
+    handleError(`Error reading JSON file at ${filePath}`, error);
   }
+};
 
-  let tableExists;
-  try {
-    tableExists = await verifyTableExists(tableName);
-  } catch (error) {
-    console.error(`Error checking if table exists: ${error}`);
-    return error;
-  }
+const extractValues = (obj) => Object.values(obj);
 
-  if (tableExists) {
-    try {
-      await truncateTable(tableName);
-      console.log(`Table "${tableName}" truncated successfully`);
-    } catch (error) {
-      console.error(`Error truncating table: ${error}`);
-      return error;
-    }
+const verifyExists = async (tableName, id = null) => {
+  let query;
+  let params;
+
+  if (id === null) {
+    query = `
+      SELECT EXISTS (
+        SELECT FROM information_schema.tables 
+        WHERE table_name = $1
+      );
+    `;
+    params = [tableName];
   } else {
-    try {
-      await createTable(tableData);
-      console.log(`Table "${tableName}" created successfully`);
-    } catch (error) {
-      console.error(`Error creating table: ${error}`);
-      return error;
-    }
+    query = `
+      SELECT EXISTS (
+        SELECT FROM ${tableName}
+        WHERE id = $1
+      );
+    `;
+    params = [id];
   }
 
   try {
-    await seedTable(tableData);
-    console.log(`Table "${tableName}" seeded successfully`);
-  } catch (error) {
-    console.error(`Error seeding table: ${error}`);
-    return error;
-  }
-};
-
-const verifyTableExists = async (tableName) => {
-  const query = `
-    SELECT EXISTS (
-      SELECT FROM information_schema.tables 
-      WHERE table_name = $1
+    const result = await db.query(query, params);
+    const exists = result.rows[0].exists;
+    console.log(
+      `${id === null ? "Table" : "Entry"} "${
+        id === null ? tableName : id
+      }" exists: ${exists}`
     );
-  `;
-  try {
-    const result = await db.query(query, [tableName]);
-    console.log(`Table "${tableName}" exists: ${result.rows[0].exists}`);
-    return result.rows[0].exists;
-  } catch (err) {
-    console.error(`Error checking if table ${tableName} exists:`, err);
-    throw err;
-  }
-};
-
-const verifyEntryExists = async (tableName, id) => {
-  const query = `
-    SELECT EXISTS (
-      SELECT FROM ${tableName}
-      WHERE id = $1
-    );
-  `;
-  try {
-    const result = await db.query(query, [id]);
-    const entryExists = result.rows[0].exists;
-    return entryExists;
+    return exists;
   } catch (error) {
-    console.error(`Error checking if entry with id ${id} exists:`, error);
+    console.error(
+      `Error checking if ${id === null ? "table" : "entry"} ${
+        id === null ? tableName : id
+      } exists:`,
+      error
+    );
     throw error;
   }
 };
@@ -98,9 +65,8 @@ const truncateTable = async (tableName) => {
   try {
     await db.query(truncateQuery);
     console.log(`Table "${tableName}" truncated successfully.`);
-  } catch (err) {
-    console.error(`Error truncating table ${tableName}:`, err);
-    throw err;
+  } catch (error) {
+    handleError(`Error truncating table ${tableName}`, error);
   }
 };
 
@@ -112,138 +78,135 @@ const createTable = async (data) => {
       .join(", ");
     await db.query(`CREATE TABLE ${tableName} (${columnsDefinition});`);
     console.log(`Table "${tableName}" created successfully.`);
-  } catch (err) {
-    console.error(`Error creating table ${tableName}:`, err);
-    throw err;
-  }
-};
-
-const fetchJson = async (filePath) => {
-  try {
-    const data = await fs.readFile(filePath, "utf-8");
-    const jsonData = JSON.parse(data);
-    return jsonData;
   } catch (error) {
-    console.error(`Error reading JSON file at ${filePath}:`, error);
-    throw error;
+    handleError(`Error creating table ${data.schema.tableName}`, error);
   }
-};
-
-const extractValues = (obj) => {
-  return Object.keys(obj).map((key) => obj[key]);
 };
 
 const seedTable = async (tableData) => {
   const { entries, schema } = tableData;
   const { tableName, columns } = schema;
 
-  const entriesToAdd = [];
   const query = `INSERT INTO ${tableName} (${columns
     .slice(1)
     .map((col) => col.name)
     .join(", ")}) VALUES (${columns
     .slice(1)
-    .map((col, index) => `$${index + 1}`)
+    .map((_, index) => `$${index + 1}`)
     .join(", ")})`;
 
-  entries.map((entry) => {
-    let singleEntryValues = extractValues(entry);
-    entriesToAdd.push(singleEntryValues);
-  });
+  try {
+    for (const entry of entries) {
+      const values = extractValues(entry);
+      await db.query(query, values);
+    }
+    console.log(`Table "${tableName}" seeded successfully.`);
+  } catch (error) {
+    handleError(`Error seeding table ${tableName}`, error);
+  }
+};
+
+const seedTestTable = async (tableName) => {
+  const tableDataPaths = {
+    events: path.join(__dirname, "/test-data/events-test-data.json"),
+    users: path.join(__dirname, "/test-data/users-test-data.json"),
+  };
+
+  const tableDataPath = tableDataPaths[tableName];
+  if (!tableDataPath) {
+    console.error(`Unknown table name: ${tableName}`);
+    return;
+  }
 
   try {
-    for (let singleEntryValues of entriesToAdd) {
-      await db.query(query, singleEntryValues);
+    const tableData = await fetchJson(tableDataPath);
+    const tableExists = await verifyExists(tableName);
+
+    if (tableExists) {
+      await truncateTable(tableName);
+    } else {
+      await createTable(tableData);
     }
-    console.log("Data inserted successfully");
+
+    await seedTable(tableData);
   } catch (error) {
-    console.error("Error inserting data:", error);
-    throw error;
+    handleError(`Error seeding test table ${tableName}`, error);
   }
 };
 
 const getEntryPropertyValue = async (tableName, id, propertyName) => {
   try {
-    const tableExists = await verifyTableExists(tableName);
-    if (!tableExists) {
-      throw new Error("Table does not exist");
-    }
+    const tableExists = await verifyExists(tableName);
+    if (!tableExists) throw new Error("Table does not exist");
 
     const query = `SELECT ${propertyName} FROM ${tableName} WHERE id = $1`;
     const result = await db.query(query, [id]);
     return result.rows[0];
   } catch (error) {
-    console.error(error);
-    throw error;
+    handleError(
+      `Error getting property value ${propertyName} from table ${tableName}`,
+      error
+    );
   }
 };
 
 const fetchEndpointsData = async () => {
   try {
-    const data = await fs.readFile(
-      path.join(__dirname, "../endpoints.json"),
-      "utf-8"
-    );
-    return JSON.parse(data);
+    return await fetchJson(path.join(__dirname, "../endpoints.json"));
   } catch (error) {
-    console.error("Error fetching endpoints data:", error);
-    throw error;
+    handleError("Error fetching endpoints data", error);
   }
 };
 
 const fetchTable = async (tableName) => {
+  const query = `SELECT * FROM ${tableName}`;
   try {
-    const query = `SELECT * FROM ${tableName}`;
     const result = await db.query(query);
     return result.rows;
   } catch (error) {
-    console.error("Error fetching table data:", error);
-    throw error;
+    handleError(`Error fetching table data from ${tableName}`, error);
   }
 };
 
 const fetchTableEntry = async (tableName, entryId) => {
+  const query = `SELECT * FROM ${tableName} WHERE id = $1`;
   try {
-    const query = `SELECT * FROM ${tableName} WHERE ${entryId} = $1`;
     const result = await db.query(query, [entryId]);
     return result.rows[0];
   } catch (error) {
-    console.error("Error fetching table data:", error);
-    throw error;
+    handleError(
+      `Error fetching entry with id ${entryId} from table ${tableName}`,
+      error
+    );
   }
 };
 
 const verifyValidEmailAddress = async (email) => {
-  console.log(email);
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
   const isEmailValidFormat = emailRegex.test(email);
-  let isEmailAvailable = true;
 
-  if (isEmailValidFormat) {
-    const query = "SELECT 1 FROM users WHERE email = $1";
+  if (!isEmailValidFormat) return false;
+
+  const query = "SELECT 1 FROM users WHERE email = $1";
+  try {
     const result = await db.query(query, [email]);
-
-    if (result.rowCount > 0) {
-      isEmailAvailable = false;
-    }
+    return result.rowCount === 0;
+  } catch (error) {
+    handleError(`Error verifying email address ${email}`, error);
   }
-
-  return isEmailAvailable && isEmailValidFormat;
 };
 
 module.exports = {
-  verifyTableExists,
   truncateTable,
   createTable,
   seedTable,
   fetchJson,
   extractValues,
-  verifyEntryExists,
   getEntryPropertyValue,
   fetchEndpointsData,
   fetchTable,
   fetchTableEntry,
   seedTestTable,
   verifyValidEmailAddress,
+  verifyExists,
 };
