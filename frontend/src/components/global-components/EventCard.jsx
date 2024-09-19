cc87122b5c8105e4ff206177271d419530c18d4e
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-19 16:42:17 +0100

    ui: add dynamically fetched images to event cards

--- frontend/src/components/global-components/EventCard.jsx
+++ frontend/src/components/global-components/EventCard.jsx
@@ -1,6 +1,12 @@
+import React from "react";
+
 const EventCard = ({ event }) => {
+  const cardStyle = {
+    backgroundImage: `url(${event.image_url})`,
+  };
+
   return (
-    <div id="event-card">
+    <div id="event-card" style={cardStyle}>
       <h2>{event.title}</h2>
       <p>{event.date}</p>
       <p>{event.time}</p>
