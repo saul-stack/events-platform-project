e4084d8411ca60cd7065277c850d9d415c3b096d
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-19 18:44:39 +0100

    feat: initialise event page for single event created the component EventPage, which displays a single event in detail, and will present options to buy a ticket and view more details about the event.

--- frontend/src/components/global-components/EventCard.jsx
+++ frontend/src/components/global-components/EventCard.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const EventCard = ({ event }) => {
+const EventCard = ({ event, eventId }) => {
+  const navigate = useNavigate();
+
   const cardStyle = {
     backgroundImage: `url(${event.image_url})`,
   };
 
+  const handleClick = (id) => {
+    console.log(`Event clicked: ${id}`);
+    navigate(`/events/${id}`);
+  };
+
   return (
-    <div id="event-card" style={cardStyle}>
+    <div
+      id="event-card"
+      style={cardStyle}
+      onClick={() => handleClick(event.id)}
+    >
       <h2>{event.title}</h2>
       <p>{event.date}</p>
       <p>{event.time}</p>
---
+++ frontend/src/components/pages/EventPage.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+import { useParams } from "react-router-dom";
+import { getEventById } from "../../../api-functions";
+
+const EventPage = () => {
+  const { eventId } = useParams();
+  const [event, setEvent] = useState(null);
+
+  useEffect(() => {
+    const fetchEvent = async () => {
+      try {
+        const eventData = await getEventById(eventId);
+        setEvent(eventData);
+      } catch (error) {
+        console.error("Error fetching event data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEvent();
+  }, [eventId]);
+
+  if (!event) {
+    return <div>Event not found</div>;
+  }
+
+  return (
+    <div className="main-content">
+      <h1>{event.title}</h1>
+      <p>{event.description}</p>
+      <p>Date: {new Date(event.date).toLocaleDateString()}</p>
+      <p>Time: {event.time}</p>
+      <p>Location: {event.location}</p>
+      <p>Advance Price: {event.advance_price}</p>
+      <p>Door Price: {event.door_price}</p>
+      <p>
+        Tickets Sold: {event.tickets_sold} / {event.tickets_total}
+      </p>
+      <p>Seated: {event.is_seated ? "Yes" : "No"}</p>
+    </div>
+  );
+};
+
+export default EventPage;
