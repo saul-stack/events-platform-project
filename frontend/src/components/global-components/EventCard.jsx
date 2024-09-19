f86edeb332ca467275af419d0f4b0a4883b2fd31
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-19 15:41:55 +0100

    ui: update events cards layout on EventsPage Made event card move dynamically in the grid layout in css.

--- frontend/src/components/global-components/EventCard.jsx
+++ frontend/src/components/global-components/EventCard.jsx
@@ -1,8 +1,7 @@
 const EventCard = ({ event }) => {
   return (
-    <div>
+    <div id="event-card">
       <h2>{event.title}</h2>
-      <p>{event.description}</p>
       <p>{event.date}</p>
       <p>{event.time}</p>
       <p>{event.location}</p>
