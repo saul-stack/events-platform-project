b892851ab1c37c2cdeebaf961e33c24ffe508953
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-21 14:08:10 +0100

    struct: import AdminPage into pages.js

--- frontend/src/components/pages.js
+++ frontend/src/components/pages.js
@@ -1,5 +1,6 @@
 export { default as AboutPage } from "./pages/AboutPage";
 export { default as AccountPage } from "./pages/AccountPage";
+export { default as AdminPage } from "./pages/AdminPage";
 export { default as EventPage } from "./pages/EventPage";
 export { default as EventsPage } from "./pages/EventsPage";
 export { default as LoginPage } from "./pages/LoginPage";
