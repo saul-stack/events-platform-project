ede26b04a745c78c3164016523a93261cdc9881f
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-22 22:40:52 +0100

    refactor: rearrange imports in AccountPage.jsx

--- frontend/src/components/pages/AccountPage.jsx
+++ frontend/src/components/pages/AccountPage.jsx
@@ -1,7 +1,7 @@
+import MyEvents from "../AccountPage/MyEvents";
+import { UserContext } from "../../contexts/UserContext";
 import { useContext } from "react";
 import { useLogout } from "../../../account-util-functions";
-import { UserContext } from "../../contexts/UserContext";
-import MyEvents from "../AccountPage/MyEvents";
 
 function AccountPage() {
   const logout = useLogout();
@@ -11,10 +11,10 @@ function AccountPage() {
   return (
     <div className="main-content">
       <div id="logout-bar">
-      <p>
-        Logged in as {role}: {user_name}
-      </p>
-      <button onClick={logout}>Logout</button>
+        <p>
+          Logged in as {role}: {user_name}
+        </p>
+        <button onClick={logout}>Logout</button>
       </div>
       <MyEvents />
     </div>
