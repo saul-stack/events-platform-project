4b626c3af3655008254bbb262e043986337adbd0
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-22 22:37:30 +0100

    ui: modulrze. EventsGrid comp. (upcoming and past) Added modularity to EventsGrid component. This means it can be used to display both upcoming and past events. This change also includes a new prop `type` which can be set to either `upcoming` or `past` to determine which events are displayed.

--- frontend/src/components/global-components/EventsGrid.jsx
+++ frontend/src/components/global-components/EventsGrid.jsx
@@ -1,8 +1,15 @@
 import EventCardSmall from "../global-components/EventCardSmall";
 
-const EventsGrid = ({ events, error }) => {
+const EventsGrid = ({ events, error, timeline }) => {
+  const titleText =
+    timeline === "upcoming"
+      ? "Upcoming Events"
+      : timeline === "past"
+      ? "Past Events"
+      : null;
   return (
     <div id="events-grid">
+      {titleText && <h1>{titleText}</h1>}
       {events ? (
         events.map((event) => <EventCardSmall key={event.id} event={event} />)
       ) : (
