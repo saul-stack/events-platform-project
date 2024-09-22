d1b45bd854865bd1cc53b426db38aee0353e76f4
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-22 22:42:04 +0100

    refactor: rearrange imports for MyEvents component

--- frontend/src/components/AccountPage/MyEvents.jsx
+++ frontend/src/components/AccountPage/MyEvents.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 
-import { getEventById } from "../../../api-functions";
-import { UserContext } from "../../contexts/UserContext";
 import EventsGrid from "../global-components/EventsGrid";
+import { UserContext } from "../../contexts/UserContext";
+import { getEventById } from "../../../api-functions";
 
 const MyEvents = () => {
   const { user } = useContext(UserContext);
