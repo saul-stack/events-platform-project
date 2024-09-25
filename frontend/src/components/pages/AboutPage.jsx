23cdee5f5f88b993940e68636e269b1144c5bfc3
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-25 16:23:26 +0100

    ui: update AboutPage with more venue information added more information about the venue to the AboutPage component.

--- frontend/src/components/pages/AboutPage.jsx
+++ frontend/src/components/pages/AboutPage.jsx
@@ -4,14 +4,34 @@ function AboutPage() {
       <div className="paragraph">
         <h1>Bar-None Club</h1>
         <p>
-          Bar-None Club is a community-led Bristol venue known for hosting live
-          music, comedy, and community events. Whether it's electronic music or
-          stand-up comedy, we have something for everyone. We also host skills
-          workshops focusing on practical skills such as carpentry and cooking
-          to help people in the community to improve their lives. Contact us to
-          hire our space.
+          Bar-None Club is a lively, community-driven venue in the heart of
+          Bristol, offering a little something for everyone.
         </p>
-        <h1>Our Ethos</h1>
+        <p>
+          We're known for hosting an exciting mix of live music, comedy, and
+          community events that bring people together. Whether you're into
+          electronic beats, live bands, or a good laugh, we've got you covered.
+        </p>
+        <p>
+          But it's not just about entertainment— we know how important it is for
+          people to grow and learn. We run hands-on workshops focussing on
+          practical skills like carpentry and cooking, giving people in the
+          community the chance to pick up new skills and improve their
+          day-to-day lives.
+        </p>
+        <h2>Hire Our Space</h2>
+        <p>
+          Need a place to hold your event? Bar-None is available for hire too.
+          Whether you're planning a party or something special for the
+          community, our space is flexible, welcoming, and ready to accommodate
+          your needs.
+        </p>
+        <p>
+          Feel free to get in touch with us for more info on hiring the space.
+          We're always happy to help and would love to have you be a part of our
+          community!
+        </p>
+        <h2>Our Ethos</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
           laoreet felis mollis, tincidunt arcu sit amet, aliquet erat. Curabitur
