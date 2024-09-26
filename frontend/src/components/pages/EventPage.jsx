779dc20bbf187ffc447c7a89e9b3c1989df2e147
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-26 17:22:21 +0100

    ui: add conditional rendering to BuyTicketForm added conditional rendering logic to display the BuyTicketForm component. It will show when the user clicks the "Buy Ticket" button on the EventPage, and also when navigated to with a click on the "Buy Ticket" button on the EventCardSmall.

--- frontend/src/components/pages/EventPage.jsx
+++ frontend/src/components/pages/EventPage.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+
+import BuyTicketForm from "../EventPage/BuyTicketForm";
 import EventCardLarge from "../EventPage/EventCardLarge";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const EventPage = () => {
+  const location = useLocation();
+  const { showBuyForm } = location.state || {};
+
+  const [showBuyTicketForm, setShowBuyTicketForm] = useState(false);
+
+  useEffect(() => {
+    if (showBuyForm) {
+      setShowBuyTicketForm(true);
+    }
+  }, [showBuyForm]);
+
+  const handleBuyButtonClick = () => {
+    setShowBuyTicketForm(!showBuyTicketForm);
+  };
+
   return (
     <div className="main-content">
-      <EventCardLarge />
+      <EventCardLarge
+        showBuyTicketForm={showBuyTicketForm}
+        handleBuyButtonClick={handleBuyButtonClick}
+      />
+      {showBuyTicketForm && (
+        <BuyTicketForm
+          showBuyTicketForm={showBuyTicketForm}
+          setShowBuyTicketForm={setShowBuyTicketForm}
+        />
+      )}
     </div>
   );
 };
