b646c07d2bb7dfc6ddf82d2f7ff2b322eab94484
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-26 17:07:30 +0100

    ui: update classes in AboutPage divided the about page into sections and update classes

--- frontend/src/components/pages/AboutPage.jsx
+++ frontend/src/components/pages/AboutPage.jsx
@@ -1,43 +1,59 @@
 function AboutPage() {
   return (
     <div className="main-content">
-      <div className="paragraph">
-        <h1>Bar-None Club</h1>
-        <p>
-          Bar-None Club is a lively, community-driven venue in the heart of
-          Bristol, offering a little something for everyone.
-        </p>
-        <p>
-          We're known for hosting an exciting mix of live music, comedy, and
-          community events that bring people together. Whether you're into
-          electronic beats, live bands, or a good laugh, we've got you covered.
-        </p>
-        <p>
-          But it's not just about entertainment— we know how important it is for
-          people to grow and learn. We run hands-on workshops focussing on
-          practical skills like carpentry and cooking, giving people in the
-          community the chance to pick up new skills and improve their
-          day-to-day lives.
-        </p>
-        <h2>Hire Our Space</h2>
-        <p>
-          Need a place to hold your event? Bar-None is available for hire too.
-          Whether you're planning a party or something special for the
-          community, our space is flexible, welcoming, and ready to accommodate
-          your needs.
-        </p>
-        <p>
-          Feel free to get in touch with us for more info on hiring the space.
-          We're always happy to help and would love to have you be a part of our
-          community!
-        </p>
-        <h2>Our Ethos</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-          laoreet felis mollis, tincidunt arcu sit amet, aliquet erat. Curabitur
-          imperdiet vel risus sed convallis. Maecenas nec est quis sapien
-          scelerisque condimentum vitae ac dui.
-        </p>
+      <div>
+        <div className="about-page-card">
+          <div className="container-topbar">
+            <h1 id="title">Bar-None Club</h1>
+          </div>
+          <div className="about-page-paragraph">
+            <div>
+              <h2>About Us</h2>
+              <p>
+                Bar-None Club is a lively, community-driven venue in the heart
+                of Bristol, offering a little something for everyone.
+              </p>
+              <p>
+                We're known for hosting an exciting mix of live music, comedy,
+                and community events that bring people together. Whether you're
+                into electronic beats, live bands, or a good laugh, we've got
+                you covered.
+              </p>
+              <p>
+                But it's not just about entertainment— we know how important it
+                is for people to grow and learn. We run hands-on workshops
+                focussing on practical skills like carpentry and cooking, giving
+                people in the community the chance to pick up new skills and
+                improve their day-to-day lives.
+              </p>
+            </div>
+            <div>
+              <h2>Hire Our Space</h2>
+              <p>
+                Need a place to hold your event? Bar-None is available for hire
+                too. Whether you're planning a party or something special for
+                the community, our space is flexible, welcoming, and ready to
+                accommodate your needs.
+              </p>
+
+              <p>
+                Feel free to get in touch with us for more info. We're always
+                happy to help and would love to have you be a part of our
+                community!
+              </p>
+            </div>
+
+            <div>
+              <h2>Our Ethos</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                laoreet felis mollis, tincidunt arcu sit amet, aliquet erat.
+                Curabitur imperdiet vel risus sed convallis. Maecenas nec est
+                quis sapien scelerisque condimentum vitae ac dui.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
