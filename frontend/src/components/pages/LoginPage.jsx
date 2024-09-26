6ff90dae1a55add33369849b27758a744adad583
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-26 17:09:36 +0100

    ui: update class structure for LoginPage

--- frontend/src/components/pages/LoginPage.jsx
+++ frontend/src/components/pages/LoginPage.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useState } from "react";
 
-import { useNavigate } from "react-router-dom";
-import { logUserIn } from "../../../api-functions";
 import { UserContext } from "../../contexts/UserContext";
+import { logUserIn } from "../../../api-functions";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -27,34 +27,37 @@ const LoginPage = () => {
 
   return (
     <div className="main-content">
-      <h1>You are not logged in.</h1>
-      <p>Login to watch and book events, and manage your booked events.</p>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        {errorMessage && <p className="error">{errorMessage}</p>}
-        <button type="submit">Login</button>
-      </form>
+      <h1 className="login-title">You are not logged in.</h1>
+
+      <div className="login-container">
+        <p>Login to watch and book events, and manage your booked events.</p>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          {errorMessage && <p className="error">{errorMessage}</p>}
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
