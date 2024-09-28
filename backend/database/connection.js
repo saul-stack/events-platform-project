0df9c4fecddb0c5ed69c38fb05973031c2130a40
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-28 13:45:21 +0100

    data: add cond. logic to ssl config in connection added logic to conditionally set ssl config in connection.js, based on the environment variable "NODE_ENV"

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,19 +1,27 @@
 const { Pool } = require("pg");
 const dotenv = require("dotenv");
 
-dotenv.config({ path: `${__dirname}/../.env.development` });
+const environment = process.env.NODE_ENV || "development";
+
+dotenv.config({ path: `${__dirname}/../.env.${environment}` });
 if (!process.env.PGDATABASE) {
   throw new Error("PGDATABASE not set");
 }
-const db = new Pool({
+
+const poolConfig = {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-  ssl: {
+};
+
+if (environment !== "development") {
+  poolConfig.ssl = {
     rejectUnauthorized: false,
-  },
-});
+  };
+}
+
+const db = new Pool(poolConfig);
 
 module.exports = db;
