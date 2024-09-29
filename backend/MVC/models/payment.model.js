6a9bca5fc56834099d21991d88125286b01d1157
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-29 20:24:34 +0100

    feat: initialise payment model Initialised a payment model for Stripe payments.

---
+++ backend/MVC/models/payment.model.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+exports.sendStripePayment = async (req, res) => {
+  const { price, title } = req.body;
+
+  const BASE_URL = process.env.BASE_URL || "http://localhost:5173";
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "gbp",
+            product_data: {
+              name: `${title}`,
+            },
+            unit_amount: price * 100,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: `${BASE_URL}/account`,
+      cancel_url: `${BASE_URL}/account`,
+    });
+
+    res.json({ id: session.id });
+  } catch (error) {
+    console.error("Error creating Stripe checkout session:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
