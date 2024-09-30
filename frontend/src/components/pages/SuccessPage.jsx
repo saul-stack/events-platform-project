1c56999730747ec985e31cc2633d7c312a5205fc
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-09-30 16:14:27 +0100

    frontend: implement useLocation to fetch queries

--- frontend/src/components/pages/SuccessPage.jsx
+++ frontend/src/components/pages/SuccessPage.jsx
@@ -1,6 +1,39 @@
-import { useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { UserContext } from "../../contexts/UserContext";
+import { bookEvent } from "../../../api-functions";
+
 const SuccessPage = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const { user, updateUser } = useContext(UserContext);
+
+  console.log(location);
+
+  const getQueryParams = (query) => {
+    return new URLSearchParams(query);
+  };
+
+  useEffect(() => {
+    const queryParams = getQueryParams(location.search);
+    const eventId = queryParams.get("eventId");
+    console.log(eventId);
+
+    if (eventId) {
+      const newUser = {
+        ...user,
+        events_booked: [...user.events_booked, Number(eventId)],
+      };
+      updateUser(newUser);
+
+      console.log(user.id, eventId);
+      bookEvent(user.id, Number(eventId));
+      console.log(newUser);
+      navigate("/account");
+    }
+  }, [location.search, user, navigate]);
+
   const handleClick = () => {
     navigate("/account");
   };
