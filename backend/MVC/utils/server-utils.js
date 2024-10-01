9a71752b084dd05d1f73f2e0402fa827090354b2
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-10-02 00:52:40 +0100

    refactor: update pingEndpoint function for clarity

--- backend/MVC/utils/server-utils.js
+++ backend/MVC/utils/server-utils.js
@@ -1,14 +1,16 @@
 const axios = require("axios");
 
-exports.pingEndpoint = async (url) => {
+exports.pingEndpoint = async (url, name) => {
+  const time = new Date().toLocaleTimeString("en-GB", { hour12: false });
+
   try {
     const response = await axios.get(url);
     console.log(
-      `Reloaded ${url} @ ${new Date().toISOString()}: Status Code ${
-        response.status
-      }.`
+      `Reloaded ${name || url} @ ${time}: Status Code: (${response.status}).`
     );
-  } catch {
-    console.error(`Error reloading ${url} @ ${new Date().toISOString()}:`);
+  } catch (error) {
+    console.error(`Error reloading ${name || url} @ ${time}:`, error.message);
   }
+};
+
 };
