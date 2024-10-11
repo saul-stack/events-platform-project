50d781308c0f01bbde82c3501c13c5ed396add95
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-10-11 14:53:51 +0100

    fix: redirect from AccountPage if not logged in added a check in AccountPage component which navigates to /login if the userContext.user has no value

--- frontend/src/components/pages/AccountPage.jsx
+++ frontend/src/components/pages/AccountPage.jsx
@@ -1,14 +1,21 @@
 import "../../styles/css/AccountPage.css";
 
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { useLogout } from "../../../account-util-functions";
 import { UserContext } from "../../contexts/UserContext";
 import MyEvents from "../AccountPage/MyEvents";
 
 function AccountPage() {
+  const navigate = useNavigate();
   const logout = useLogout();
   const { user, setUser } = useContext(UserContext);
   const { role, user_name } = user;
+
+  if (!user_name) {
+    setUser({});
+    navigate("/login");
+  }
 
   return (
     <main className="main-content">
