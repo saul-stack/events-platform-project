2820fa55fff955c70af7992f7df9511f976cf7ea
Saul Smith <125879463+saul-stack@users.noreply.github.com>
2024-10-29 20:56:26 +0000

    fix: update postNewEvent function for users_booked

--- backend/MVC/models/events.models.js
+++ backend/MVC/models/events.models.js
@@ -59,7 +59,7 @@ const postEvent = async (newEvent) => {
     const values = extractValues(newEvent);
 
     await db.query(
-      "INSERT INTO events (title, date, time, description, event_type, advance_price, door_price, tickets_total, tickets_sold, is_seated, is_ticketed, is_recurring, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
+      "INSERT INTO events (title, date, time, description, event_type, advance_price, door_price, tickets_total, tickets_sold, is_seated, is_ticketed, is_recurring, image_url, users_booked) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)",
       values
     );
   } catch (error) {
